1fbeae7e82496265c4fe8381feeffd6d15cd11e9
martyan <martin.juzl@gmail.com>
2019-11-22 13:42:06 +0100

    makeThumb sharp lib

--- src/functions/makeThumb.js
+++ src/functions/makeThumb.js
@@ -2,83 +2,57 @@ const functions = require('firebase-functions')
 const mkdirp = require('mkdirp-promise')
 const admin = require('firebase-admin')
 const spawn = require('child-process-promise').spawn
-const path = require('path')
-const os = require('os')
-const fs = require('fs')
+const { join, dirname } = require('path')
+const { tmpdir } = require('os')
+const sharp = require('sharp')
+const fs = require('fs-extra')
 
-const THUMB_PATH = 'photos/thumbs/'
-const THUMB_MAX_HEIGHT = 200
-const THUMB_MAX_WIDTH = 200
+module.exports = functions.storage
+    .object()
+    .onFinalize(async object => {
+        const bucket = admin.storage().bucket(object.bucket)
+        // const bucket = gcs.bucket(object.bucket);
+        const filePath = object.name;
+        const fileName = filePath.split('/').pop();
+        const bucketDir = dirname(filePath);
 
-module.exports = functions.storage.object().onFinalize(async(object) => {
-    const filePath = object.name
-    // const fileDir = path.dirname(filePath)
-    const fileName = path.basename(filePath)
-    const thumbFilePath = path.join(THUMB_PATH, fileName)
-    const contentType = object.contentType
+        const workingDir = join(tmpdir(), 'thumbs');
+        const tmpFilePath = join(workingDir, 'source.png');
 
-    // only images accepted
-    if(!contentType.startsWith('image/')) return console.log('Not an image.', contentType)
+        if (fileName.includes('thumb@') || !object.contentType.includes('image')) {
+            console.log('exiting function');
+            return false;
+        }
 
-    // make thumb only from full res photos
-    if(!filePath.startsWith('photos/full/')) return console.log('Thumb not needed.', filePath)
+        // 1. Ensure thumbnail dir exists
+        await fs.ensureDir(workingDir);
 
-    const tempFile = path.join(os.tmpdir(), THUMB_PATH, fileName)
-    const tempThumbFile = path.join(os.tmpdir(), THUMB_PATH, `THUMB_${fileName}`)
-    const tempDir = path.dirname(tempThumbFile)
+        // 2. Download Source File
+        await bucket.file(filePath).download({
+            destination: tmpFilePath
+        });
 
-    // const thumbFilePath = path.normalize(path.join(fileDir, `${fileName}`))
-    // const tempLocalFile = path.join(os.tmpdir(), filePath)
-    // const tempLocalDir = path.dirname(tempLocalFile)
-    // const tempLocalThumbFile = path.join(os.tmpdir(), thumbFilePath)
-    //
-    // console.log(filePath, thumbFilePath, tempLocalFile, tempLocalThumbFile)
+        // 3. Resize the images and define an array of upload promises
+        const sizes = [64, 128, 256];
 
-    // Cloud Storage files.
-    const bucket = admin.storage().bucket(object.bucket)
-    const file = bucket.file(filePath)
-    const thumbFile = bucket.file(thumbFilePath)
-    const metadata = {
-        contentType: contentType,
-        // To enable Client-side caching you can set the Cache-Control headers here. Uncomment below.
-        // 'Cache-Control': 'public,max-age=3600',
-    }
+        const uploadPromises = sizes.map(async size => {
+            const thumbName = `thumb@${size}_${fileName}`;
+            const thumbPath = join(workingDir, thumbName);
 
-    // Create the temp directory where the storage file will be downloaded.
-    await mkdirp(tempDir)
+            // Resize source image
+            await sharp(tmpFilePath)
+                .resize(size, size)
+                .toFile(thumbPath);
 
-    // Download file from bucket.
-    await file.download({ destination: tempFile })
-    console.log('The file has been downloaded to', tempFile)
+            // Upload to GCS
+            return bucket.upload(thumbPath, {
+                destination: join(bucketDir, thumbName)
+            });
+        });
 
-    // Generate a thumbnail using ImageMagick.
-    await spawn('convert', [tempFile, '-thumbnail', `${THUMB_MAX_WIDTH}x${THUMB_MAX_HEIGHT}>`, tempThumbFile], { capture: ['stdout', 'stderr'] })
-    console.log('Thumbnail created at', tempThumbFile)
+        // 4. Run the upload operations
+        await Promise.all(uploadPromises);
 
-    // Uploading the Thumbnail.
-    await bucket.upload(tempThumbFile, { destination: thumbFilePath, metadata: metadata })
-    console.log('Thumbnail uploaded to Storage at', thumbFilePath)
-
-    // Once the image has been uploaded delete the local files to free up disk space.
-    fs.unlinkSync(tempFile)
-    fs.unlinkSync(tempThumbFile)
-
-    // Get the Signed URLs for the thumbnail and original image.
-    const config = {
-        action: 'read',
-        expires: '03-01-2500',
-    }
-    const results = await Promise.all([
-        thumbFile.getSignedUrl(config),
-        file.getSignedUrl(config),
-    ])
-    console.log('Got Signed URLs.')
-
-    const thumbResult = results[0]
-    const originalResult = results[1]
-    const thumbFileUrl = thumbResult[0]
-    const fileUrl = originalResult[0]
-
-    return admin.firestore().collection('thumbs').add({ path: fileUrl, thumbnail: thumbFileUrl })
-    return console.log('Thumbnail URLs saved to database.')
-})
+        // 5. Cleanup remove the tmp/thumbs from the filesystem
+        return fs.remove(workingDir);
+    });
